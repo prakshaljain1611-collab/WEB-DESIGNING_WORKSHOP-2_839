8efb16ba0bba16cf06d1f2bf7dec8c963e7f7cbb
prakshaljain1611-collab <prakshaljain1611@gmail.com>
2026-05-17 15:40:33 +0530

    work

---
+++ EXPERIMENT/experiment-7/vite-project/src/App.jsx
@@ -0,0 +1,8 @@
+function App(){
+return(
+  <div className="App_Class">
+    <h1>hello cse22</h1>
+    <p>welcome</p>
+  </div>
+):
+}
---
+++ function and object/app.js
@@ -0,0 +1,9 @@
+let numbers=[1,2,3,4,5,6];
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+// arrow function with one parameters
+let square = x =>x*x;
+console.log(square(4));
